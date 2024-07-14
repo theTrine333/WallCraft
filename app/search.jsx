0508fdbcea41001d516725872447ac77174142ae
Erick Ronald <148716108+theTrine333@users.noreply.github.com>
2024-07-15 01:12:57 +0300

    Update search.jsx

--- app/search.jsx
+++ app/search.jsx
@@ -1,4 +1,4 @@
-import { ActivityIndicator, Image,StyleSheet, TouchableOpacity,Text,FlatList, TextInput,View } from 'react-native'
+import { ActivityIndicator, Image,StyleSheet, TouchableOpacity,Text,FlatList, Dimensions,TextInput,View } from 'react-native'
 import {React,useState } from 'react'
 import * as Fetcher from '@/api/fetcher';
 import { useNavigation } from '@react-navigation/native';
@@ -10,34 +10,52 @@ const Search = () => {
     const [isFetched,setFetched] = useState(false)
     const [loading,setLoading] = useState(false)
     const navigation = useNavigation();
-
-    function ImageCards({Poster,ImageUrl}){
+    let timeoutId = null;
+    function ResultCard({data}){
       return(
           <TouchableOpacity onPress={() => {
-              navigation.push('Viewer',{
-                  Poster:Poster,
-                  ImageUrl:ImageUrl
-              })
-              
+            var tag = Fetcher.extractTags(`${data.url}`)
+            navigation.push("seeAll",{
+              Tag:tag
+            })
           }} style={styles.Card}>
-              <Image
-                  source={{uri: `${Poster}`}}
-                  style={{width:105,height:150,borderRadius:10}}
+            <View style={{flexDirection:'row',width:width*0.8}}>
+              <View style={{marginLeft:10,marginRight:10,marginBottom:10,height:height*0.1,width:width*0.2}}>
+                <Image
+                  source={{uri: `${data.img}`}}
+                  style={{flex:1,borderRadius:8}}
                   resizeMode='cover'
-              />
+                />
+              </View>
+              <View>
+                <Text numberOfLines={2} style={{color:'white',fontWeight:'bold',marginTop:10}}>{data.title.replace(/<\/?b>/g, "")}</Text>
+                <Text style={{color:'white'}}>{data.img_count}</Text>
+              </View>
+            </View>
+              
           </TouchableOpacity>
       )
     }
+
+    const handleTextChange = (inputText) => {
+      setText(inputText);
+      setLoading(true)
+      clearTimeout(timeoutId); // Clear previous timeout
+      timeoutId = setTimeout(() => {
+        Fetcher.getSearch(text,setTags).then(data =>{
+          setLoading(false);
+        })
+      }, 700);
+    };
 
   return (
     <View style={styles.container}>
       <TextInput style={styles.searchBox} 
         placeholder='Search for a tag'
-        onChangeText={text =>{setText(text)}}
+        onChangeText={handleTextChange}
         onSubmitEditing={async ()=>{
           if (text != ''){
               setLoading(true)
-
               Fetcher.get_images_search(text).then(tags => {
                 if (tags.length === 0) {
                   setEmpty(true)
@@ -47,8 +65,7 @@ const Search = () => {
                   setEmpty(false)
                   setFetched(true)  
                   setLoading(false)
-                }
-                
+                }                
               }).catch(error => console.error(error))  
           }
         }}
@@ -65,11 +82,8 @@ const Search = () => {
           <View style={{flex:1,flexDirection:'column',margin:10}}>
             <FlatList
               data={Tags}
-              renderItem={({item}) => <ImageCards Poster={item.Image} ImageUrl={item.Image_url}/>}
-              keyExtractor={(item, index) => index.toString()}
-              numColumns={3}
+              renderItem={({item}) => <ResultCard data={item}/>}
               showsVerticalScrollIndicator={false}
-              vertical
             />
           </View>
         )
@@ -79,7 +93,7 @@ const Search = () => {
 }
 
 export default Search
-
+const {height,width} = Dimensions.get("window");
 const styles = StyleSheet.create({
     container:{
         paddingTop:45,
@@ -92,8 +106,5 @@ const styles = StyleSheet.create({
         height:38,
         borderRadius:8,
         paddingLeft:10
-    },Card:{
-      marginLeft:10,
-      marginBottom:10
     }
 })

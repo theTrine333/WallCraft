831d54f622a73002f6200a52fd6c01a3ae4f6186
theTrine333 <trinekaka@gmail.com>
2024-11-06 18:38:29 +0300

    removed unwanted plugins

--- .expo/types/router.d.ts
+++ .expo/types/router.d.ts
@@ -6,7 +6,7 @@ export * from 'expo-router';
 declare module 'expo-router' {
   export namespace ExpoRouter {
     export interface __routes<T extends string = string> extends Record<string, unknown> {
-      StaticRoutes: `/Specific` | `/TopTabs` | `/TopTabs/anime` | `/TopTabs/black` | `/TopTabs/cars` | `/TopTabs/dark` | `/Viewer` | `/_sitemap` | `/mainScreen` | `/search` | `/seeAll`;
+      StaticRoutes: never;
       DynamicRoutes: never;
       DynamicRouteTemplate: never;
     }
